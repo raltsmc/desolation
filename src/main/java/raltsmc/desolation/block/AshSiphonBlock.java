4c2285d5ca6145ba895f4d7cbe29908d18d991af
raltsmc <72952333+raltsmc@users.noreply.github.com>
2021-02-03 03:26:19 -0500

    Ash Siphon functionality

--- src/main/java/raltsmc/desolation/block/AshSiphonBlock.java
+++ src/main/java/raltsmc/desolation/block/AshSiphonBlock.java
@@ -1,9 +1,19 @@
 package raltsmc.desolation.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.world.BlockView;
+import raltsmc.desolation.block.entity.AshSiphonBlockEntity;
 
-public class AshSiphonBlock extends Block {
+public class AshSiphonBlock extends Block implements BlockEntityProvider {
+
     public AshSiphonBlock(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public BlockEntity createBlockEntity(BlockView world) {
+        return new AshSiphonBlockEntity();
     }
 }
--- src/main/java/raltsmc/desolation/block/entity/AshSiphonBlockEntity.java
+++ src/main/java/raltsmc/desolation/block/entity/AshSiphonBlockEntity.java
@@ -1,23 +1,26 @@
 package raltsmc.desolation.block.entity;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Tickable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.biome.Biome;
 import raltsmc.desolation.Desolation;
 import raltsmc.desolation.registry.DesolationBlockEntities;
+import raltsmc.desolation.registry.DesolationBlocks;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Random;
 
 public class AshSiphonBlockEntity extends BlockEntity implements Tickable {
     private boolean isActive;
-    private int radius;
-    private int dHeight;
-    private float tickPercentChance = 1.5f;
+    private int radius = 5;
+    private int dHeight = 2;
+    private float tickPercentChance = 0.7f;
     private final Random random;
 
     private boolean correctBiome;
@@ -26,27 +29,53 @@ public class AshSiphonBlockEntity extends BlockEntity implements Tickable {
         super(DesolationBlockEntities.ASH_SIPHON_BLOCK_ENTITY);
         isActive = false;
         random = new Random();
-
-        Identifier currentBiome = this.world.getRegistryManager().get(Registry.BIOME_KEY).getId(this.world.getBiome(pos));
-        correctBiome =
-                ((Objects.equals(currentBiome, Desolation.id("charred_forest"))
-                || Objects.equals(currentBiome, Desolation.id("charred_forest_small"))
-                || Objects.equals(currentBiome, Desolation.id("charred_forest_clearing")))
-                && this.pos.getY() >= world.getSeaLevel() - 10);
     }
 
     @Override
     public void tick() {
-        if (correctBiome) {
-            if (getWorld().isReceivingRedstonePower(pos)) {
-                isActive = true;
-            } else {
-                isActive = false;
-            }
+        if (!world.isClient) {
+            Identifier currentBiome = this.getWorld().getRegistryManager().get(Registry.BIOME_KEY).getId(this.world.getBiome(pos));
+            correctBiome =
+                    ((Objects.equals(currentBiome, Desolation.id("charred_forest"))
+                            || Objects.equals(currentBiome, Desolation.id("charred_forest_small"))
+                            || Objects.equals(currentBiome, Desolation.id("charred_forest_clearing")))
+                            && this.pos.getY() >= world.getSeaLevel() - 10);
+            if (correctBiome) {
+                if (getWorld().isReceivingRedstonePower(pos)) {
+                    isActive = true;
+                } else {
+                    isActive = false;
+                }
 
-            if (isActive && random.nextInt(100) < tickPercentChance) {
-                List<BlockPos> openBlocks;
-                //BlockPos.findClosest()
+                float b = random.nextFloat() * 100;
+                if (isActive && b < tickPercentChance) {
+                    List<BlockPos> openBlocks = new ArrayList<>();
+                    BlockPos.stream(pos.getX() - radius, pos.getY(), pos.getZ() - radius, pos.getX() + radius, pos.getY() + dHeight, pos.getZ() + radius)
+                            .filter(blockPos -> world.getBlockState(blockPos.down()).isSolidBlock(world, blockPos.down()) && (world.getBlockState(blockPos).isAir() || (world.getBlockState(blockPos).getBlock() == DesolationBlocks.ASH_LAYER_BLOCK && world.getBlockState(blockPos).get(Properties.LAYERS) < 8)))
+                            .forEach(e -> openBlocks.add(e.toImmutable()));
+                    for (int i = openBlocks.size() - 1; i >= 0; i--) {
+                        BlockPos blockPos = openBlocks.get(i);
+                        if (!(world.getBlockState(blockPos.down()).isSolidBlock(world, blockPos.down())
+                                && (!world.getBlockState(blockPos).isSolidBlock(world, blockPos) || (world.getBlockState(blockPos).getBlock() == DesolationBlocks.ASH_LAYER_BLOCK && world.getBlockState(blockPos).get(Properties.LAYERS) < 8)))) {
+                            openBlocks.remove(i);
+                        }
+                    }
+
+                    if (openBlocks.size() > 0) {
+                        BlockPos toUpdate = openBlocks.get(random.nextInt(openBlocks.size()));
+                        BlockState stateToUpdate = world.getBlockState(toUpdate);
+
+                        if (stateToUpdate.getBlock() == DesolationBlocks.ASH_LAYER_BLOCK) {
+                            System.out.println("Updating ash block; current layer count: " + stateToUpdate.get(Properties.LAYERS));
+                            int newLayers = stateToUpdate.get(Properties.LAYERS) + 1;
+                            if (newLayers > 8) newLayers = 8;
+                            world.setBlockState(toUpdate, stateToUpdate.with(Properties.LAYERS, newLayers));
+                        } else {
+                            System.out.println("Updating non-solid block");
+                            world.setBlockState(toUpdate, DesolationBlocks.ASH_LAYER_BLOCK.getDefaultState());
+                        }
+                    }
+                }
             }
         }
     }
