ed4496cd51534c95f0ed96bd5212f0b4326904b9
raltsmc <72952333+raltsmc@users.noreply.github.com>
2021-02-06 09:02:52 -0500

    Readded cinder dash (what even is a packet)
    (cherry picked from commit 4ecac98897ac7c0a9e102d1c21e1d8f22692e222)

--- src/main/java/raltsmc/desolation/init/client/DesolationClient.java
+++ src/main/java/raltsmc/desolation/init/client/DesolationClient.java
@@ -6,6 +6,7 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.render.RenderLayer;
@@ -22,7 +23,7 @@ import raltsmc.desolation.registry.DesolationItems;
 @Environment(EnvType.CLIENT)
 public class DesolationClient implements ClientModInitializer {
 
-    //public static KeyBinding cinderDashBinding;
+    public static KeyBinding cinderDashBinding;
 
     @Override
     public void onInitializeClient() {
@@ -47,11 +48,11 @@ public class DesolationClient implements ClientModInitializer {
     }
 
     static {
-        /*cinderDashBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+        cinderDashBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.desolation.cinder_dash",
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_V,
                 "category.desolation.cat"
-        ));*/
+        ));
     }
 }
