57b94462e57a0ee9bb13aa0baae5222831b4215e
raltsmc <72952333+raltsmc@users.noreply.github.com>
2021-02-22 20:59:16 -0500

    Rearranged creative tab, changed more textures

--- src/main/java/raltsmc/desolation/registry/DesolationBlocks.java
+++ src/main/java/raltsmc/desolation/registry/DesolationBlocks.java
@@ -15,26 +15,26 @@ import net.minecraft.util.registry.Registry;
 
 public final class DesolationBlocks {
 
+    public static final Block CHARRED_SOIL = register(new Block(FabricBlockSettings.of(Material.SOIL).hardness(0.5f).sounds(BlockSoundGroup.GRAVEL).breakByTool(FabricToolTags.SHOVELS)), "charred_soil", true);
     public static final Block EMBER_BLOCK = register(new EmberBlock(FabricBlockSettings.of(Material.STONE).hardness(1.5f).lightLevel(8).sounds(BlockSoundGroup.STONE).breakByTool(FabricToolTags.PICKAXES, 0)), "ember_block", true);
     public static final Block ASH_BLOCK = register(new AshBlock(FabricBlockSettings.of(Material.AGGREGATE).hardness(0.5f).sounds(BlockSoundGroup.SAND).breakByTool(FabricToolTags.SHOVELS)), "ash_block", true);
     public static final Block ASH_LAYER_BLOCK = register(new AshLayerBlock(FabricBlockSettings.of(Material.AGGREGATE).hardness(0.3f).sounds(BlockSoundGroup.SAND).breakByTool(FabricToolTags.SHOVELS)), "ash", true);
+    public static final Block SCORCHED_TUFT = register(new ScorchedTuftBlock(FabricBlockSettings.of(Material.REPLACEABLE_PLANT).breakInstantly().nonOpaque().noCollision().sounds(BlockSoundGroup.CROP)), "scorched_tuft", true);
+    public static final Block ASH_BRAMBLE = register(new AshBrambleBlock(FabricBlockSettings.of(Material.REPLACEABLE_PLANT).hardness(0.3f).noCollision().nonOpaque().sounds(BlockSoundGroup.CROP).breakByTool(FabricToolTags.SWORDS)), "ash_bramble", true);
+    // TODO make branches decay properly (extend LeavesBlock without the ugly rendering)
+    public static final Block CHARRED_BRANCHES = register(new Block(FabricBlockSettings.of(Material.LEAVES).hardness(0.3f).nonOpaque().sounds(BlockSoundGroup.VINE).breakByTool(FabricToolTags.SWORDS).allowsSpawning((state, world, pos, entityType) -> { return false; })), "charred_branches", true);
     public static final Block CHARRED_LOG = register(new PillarBlock(FabricBlockSettings.of(Material.WOOD).hardness(1.8f).sounds(BlockSoundGroup.BASALT).breakByTool(FabricToolTags.AXES)), "charred_log", true);
     public static final Block CHARRED_PLANKS = register(new Block(FabricBlockSettings.of(Material.WOOD, MaterialColor.GRAY).strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD)), "charred_planks", true);
     // TODO add signs (and add them to non_flammable_wood, wall_signs, and standing_signs tags) (and add loot table +recipe)
     //public static final Block CHARRED_SIGN = register(new SignBlock(FabricBlockSettings.of(Material.WOOD,MaterialColor.GRAY).noCollision().strength(1.0f).sounds(BlockSoundGroup.WOOD)));
+    public static final Block CHARRED_SLAB = register(new SlabBlock(FabricBlockSettings.of(Material.WOOD, MaterialColor.GRAY).strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD)), "charred_slab", true);
+    public static final Block CHARRED_STAIRS = register(new DesolationStairsBlock(CHARRED_PLANKS.getDefaultState(), Settings.copy(CHARRED_PLANKS)), "charred_stairs", true);
     public static final Block CHARRED_PRESSURE_PLATE = register(new DesolationPressurePlateBlock(ActivationRule.EVERYTHING, FabricBlockSettings.of(Material.WOOD, MaterialColor.GRAY).noCollision().strength(0.5f).sounds(BlockSoundGroup.WOOD)), "charred_pressure_plate", true);
     public static final Block CHARRED_TRAPDOOR = register(new DesolationTrapdoorBlock(FabricBlockSettings.of(Material.WOOD, MaterialColor.GRAY).strength(3.0f).sounds(BlockSoundGroup.WOOD).nonOpaque().allowsSpawning((state, world, pos, entityType) -> { return false; })), "charred_trapdoor", true);
-    public static final Block CHARRED_BUTTON = register(new DesolationWoodenButtonBlock(FabricBlockSettings.of(Material.SUPPORTED).noCollision().strength(0.5f).sounds(BlockSoundGroup.WOOD)), "charred_button", true);
-    public static final Block CHARRED_STAIRS = register(new DesolationStairsBlock(CHARRED_PLANKS.getDefaultState(), Settings.copy(CHARRED_PLANKS)), "charred_stairs", true);
-    public static final Block CHARRED_SLAB = register(new SlabBlock(FabricBlockSettings.of(Material.WOOD, MaterialColor.GRAY).strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD)), "charred_slab", true);
-    public static final Block CHARRED_FENCE_GATE = register(new FenceGateBlock(FabricBlockSettings.of(Material.WOOD, MaterialColor.GRAY).strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD)), "charred_fence_gate", true);
     public static final Block CHARRED_FENCE = register(new FenceBlock(FabricBlockSettings.of(Material.WOOD, MaterialColor.GRAY).strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD)), "charred_fence", true);
+    public static final Block CHARRED_FENCE_GATE = register(new FenceGateBlock(FabricBlockSettings.of(Material.WOOD, MaterialColor.GRAY).strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD)), "charred_fence_gate", true);
+    public static final Block CHARRED_BUTTON = register(new DesolationWoodenButtonBlock(FabricBlockSettings.of(Material.SUPPORTED).noCollision().strength(0.5f).sounds(BlockSoundGroup.WOOD)), "charred_button", true);
     public static final Block CHARRED_DOOR = register(new DesolationDoorBlock(FabricBlockSettings.of(Material.WOOD, MaterialColor.GRAY).strength(3.0f).sounds(BlockSoundGroup.WOOD).nonOpaque()), "charred_door", false);
-    // TODO make branches decay properly (extend LeavesBlock without the ugly rendering)
-    public static final Block CHARRED_BRANCHES = register(new Block(FabricBlockSettings.of(Material.LEAVES).hardness(0.3f).nonOpaque().sounds(BlockSoundGroup.VINE).breakByTool(FabricToolTags.SWORDS).allowsSpawning((state, world, pos, entityType) -> { return false; })), "charred_branches", true);
-    public static final Block ASH_BRAMBLE = register(new AshBrambleBlock(FabricBlockSettings.of(Material.REPLACEABLE_PLANT).hardness(0.3f).noCollision().nonOpaque().sounds(BlockSoundGroup.CROP).breakByTool(FabricToolTags.SWORDS)), "ash_bramble", true);
-    public static final Block CHARRED_SOIL = register(new Block(FabricBlockSettings.of(Material.SOIL).hardness(0.5f).sounds(BlockSoundGroup.GRAVEL).breakByTool(FabricToolTags.SHOVELS)), "charred_soil", true);
-    public static final Block SCORCHED_TUFT = register(new ScorchedTuftBlock(FabricBlockSettings.of(Material.REPLACEABLE_PLANT).breakInstantly().nonOpaque().noCollision().sounds(BlockSoundGroup.CROP)), "scorched_tuft", true);
     public static final Block CINDERFRUIT_PLANT = register(new CinderfruitPlantBlock(FabricBlockSettings.of(Material.PLANT).hardness(0.1f).lightLevel(10).nonOpaque().noCollision().sounds(BlockSoundGroup.CROP)), "cinderfruit_plant", false);
 
     static void init() {
--- src/main/java/raltsmc/desolation/registry/DesolationItems.java
+++ src/main/java/raltsmc/desolation/registry/DesolationItems.java
@@ -15,10 +15,14 @@ public final class DesolationItems {
 
     public static final Item CHARRED_DOOR = register((BlockItem)(new TallBlockItem(DesolationBlocks.CHARRED_DOOR, (new FabricItemSettings()).group(DesolationMod.DSL_GROUP))), "charred_door");
     public static final Item CHARCOAL_BIT = register(new Item(new Item.Settings().group(DesolationMod.DSL_GROUP)), "charcoal_bit");
-    public static final Item GLASS_SHARD = register(new Item(new Item.Settings().group(DesolationMod.DSL_GROUP)), "glass_shard");
     public static final Item ASH_PILE = register(new AshItem(new Item.Settings().group(DesolationMod.DSL_GROUP)), "ash_pile");
-    public static final Item ACTIVATED_CHARCOAL = register(new Item(new Item.Settings().group(DesolationMod.DSL_GROUP)), "activated_charcoal");
+    //public static final Item GLASS_SHARD = register(new Item(new Item.Settings().group(DesolationMod.DSL_GROUP)), "glass_shard");
     public static final Item PRIMED_ASH = register(new Item(new Item.Settings().group(DesolationMod.DSL_GROUP)), "primed_ash");
+    public static final Item ACTIVATED_CHARCOAL = register(new Item(new Item.Settings().group(DesolationMod.DSL_GROUP)), "activated_charcoal");
+    public static final Item AIR_FILTER = register(new Item(new Item.Settings().group(DesolationMod.DSL_GROUP)), "air_filter");
+    public static final Item MASK = register(new Item(new FabricItemSettings().group(DesolationMod.DSL_GROUP).equipmentSlot(itemStack -> EquipmentSlot.HEAD).maxCount(1)), "mask");
+    public static final Item GOGGLES = register(new Item(new FabricItemSettings().group(DesolationMod.DSL_GROUP).equipmentSlot(itemStack -> EquipmentSlot.HEAD).maxCount(1)), "goggles");
+    public static final Item MASK_GOGGLES = register(new Item(new FabricItemSettings().group(DesolationMod.DSL_GROUP).equipmentSlot(itemStack -> EquipmentSlot.HEAD).maxCount(1)), "mask_and_goggles");
     public static final Item CINDERFRUIT = register(new Item(new Item.Settings().group(DesolationMod.DSL_GROUP).food(new FoodComponent.Builder()
                     .hunger(5)
                     .saturationModifier(6.5F)
@@ -29,11 +33,7 @@ public final class DesolationItems {
     public static final Item INFUSED_POWDER = register(new Item(new Item.Settings().group(DesolationMod.DSL_GROUP)), "infused_powder");
     public static final Item HEART_OF_CINDER = register(new CinderHeartItem(new Item.Settings().rarity(Rarity.RARE).group(DesolationMod.DSL_GROUP)), "heart_of_cinder");
 
-    public static final Item AIR_FILTER = register(new Item(new Item.Settings().group(DesolationMod.DSL_GROUP)), "air_filter");
 
-    public static final Item MASK = register(new Item(new FabricItemSettings().group(DesolationMod.DSL_GROUP).equipmentSlot(itemStack -> EquipmentSlot.HEAD).maxCount(1)), "mask");
-    public static final Item GOGGLES = register(new Item(new FabricItemSettings().group(DesolationMod.DSL_GROUP).equipmentSlot(itemStack -> EquipmentSlot.HEAD).maxCount(1)), "goggles");
-    public static final Item MASK_GOGGLES = register(new Item(new FabricItemSettings().group(DesolationMod.DSL_GROUP).equipmentSlot(itemStack -> EquipmentSlot.HEAD).maxCount(1)), "mask_and_goggles");
 
     public static final Item SPAWN_EGG_ASH_SCUTTLER = register(new SpawnEggItem(DesolationEntities.ASH_SCUTTLER,
             0x111111, 0xff7b00, new Item.Settings().group(ItemGroup.MISC)), "ash_scuttler_spawn_egg");
