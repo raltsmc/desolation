8cc3f64bcd8f6108493152e3b457f2786df6664a
raltsmc <72952333+raltsmc@users.noreply.github.com>
2021-03-27 14:26:34 -0400

    Added head tracking for mobs, name & recipe changes

--- src/main/java/raltsmc/desolation/entity/model/AshScuttlerEntityModel.java
+++ src/main/java/raltsmc/desolation/entity/model/AshScuttlerEntityModel.java
@@ -3,7 +3,10 @@ package raltsmc.desolation.entity.model;
 import net.minecraft.util.Identifier;
 import raltsmc.desolation.Desolation;
 import raltsmc.desolation.entity.AshScuttlerEntity;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class AshScuttlerEntityModel extends AnimatedGeoModel<AshScuttlerEntity> {
 
@@ -21,5 +24,17 @@ public class AshScuttlerEntityModel extends AnimatedGeoModel<AshScuttlerEntity> 
 	public Identifier getAnimationFileLocation(AshScuttlerEntity object)
 	{
 		return Desolation.id("animations/ash_scuttler.json");
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	public void setLivingAnimations(AshScuttlerEntity entity, Integer uniqueID, AnimationEvent customPredicate) {
+		super.setLivingAnimations(entity, uniqueID, customPredicate);
+		IBone head = this.getAnimationProcessor().getBone("head");
+		EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
+		if (head != null) {
+			head.setRotationX(extraData.headPitch * ((float) Math.PI / 360F));
+			head.setRotationY(extraData.netHeadYaw * ((float) Math.PI / 340F));
+		}
 	}
 }
--- src/main/java/raltsmc/desolation/entity/model/BlackenedEntityModel.java
+++ src/main/java/raltsmc/desolation/entity/model/BlackenedEntityModel.java
@@ -3,7 +3,10 @@ package raltsmc.desolation.entity.model;
 import net.minecraft.util.Identifier;
 import raltsmc.desolation.Desolation;
 import raltsmc.desolation.entity.BlackenedEntity;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class BlackenedEntityModel<T> extends AnimatedGeoModel<BlackenedEntity> {
 
@@ -22,4 +25,16 @@ public class BlackenedEntityModel<T> extends AnimatedGeoModel<BlackenedEntity> {
     {
         return Desolation.id("animations/blackened.json");
     }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	public void setLivingAnimations(BlackenedEntity entity, Integer uniqueID, AnimationEvent customPredicate) {
+    	super.setLivingAnimations(entity, uniqueID, customPredicate);
+		IBone head = this.getAnimationProcessor().getBone("head");
+		EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
+		if (head != null) {
+			head.setRotationX(extraData.headPitch * ((float) Math.PI / 360F));
+			head.setRotationY(extraData.netHeadYaw * ((float) Math.PI / 340F));
+		}
+	}
 }
