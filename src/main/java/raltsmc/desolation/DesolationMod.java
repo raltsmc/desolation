abbccf17f876f48aecc4226f6fd9eb2cfa9325be
raltsmc <72952333+raltsmc@users.noreply.github.com>
2021-10-25 14:54:23 -0400

    Removed redundant autoconfig include (raltsmc/mobblindness#1)

--- src/main/java/raltsmc/desolation/DesolationMod.java
+++ src/main/java/raltsmc/desolation/DesolationMod.java
@@ -1,7 +1,7 @@
 package raltsmc.desolation;
 
-import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
-import me.sargunvohra.mcmods.autoconfig1u.serializer.JanksonConfigSerializer;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.OverworldBiomes;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
--- src/main/java/raltsmc/desolation/config/DesolationModMenu.java
+++ src/main/java/raltsmc/desolation/config/DesolationModMenu.java
@@ -2,7 +2,7 @@ package raltsmc.desolation.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
